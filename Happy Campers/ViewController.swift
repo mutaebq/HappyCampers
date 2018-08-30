4fff65d2b714dd99949f87fb276684f43d8a123b
sodok <sodok@fidelium10.com>
2018-08-30 17:07:48 -0400

    Initial Commit

---
+++ Happy Campers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Happy Campers
+//
+//  Created by axMxe on 8/30/18.
+//  Copyright © 2018 axMxe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
